d9a01dbfa1f5fa08754f15d5a516725cbad71f9a
Mr kin <kiengithub@gmail.com>
2024-12-02 14:15:11 +0700

    thêm phân trang cho tra cứu tằi liệu

--- AI_health/app/urls.py
+++ AI_health/app/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import views
 from .apis import predictAPI
-from django.conf.urls.static import static
 
 viewsURL = [
     path("", views.index, name="index"),
     path("prediction/chat/", views.chat_page, name="chat_page"),
     path("prediction/select/", views.select_page, name="select_page"),
-    path("post/", views.post_page, name="post_page"),
-    path("post/view/", views.post_view_page, name="post_view_page"),
+    path("post/page/<int:pageNumber>", views.post_page, name="post_page"),
+    path("post/page/", views.post_page, name="post_page"),
+    path("post/view/<int:postId>", views.post_view_page, name="post_view_page"),
 ]
 
 apisURL = [
--- AI_health/app/views/views.py
+++ AI_health/app/views/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 from ..models import Post
+from django.core.paginator import Paginator
 
 @require_GET
 def index(request):
@@ -14,16 +15,26 @@ def chat_page(request):
 def select_page(request):
     return render(request, "select-page.html", {})
 
+pageSize = 4
 @require_GET
-def post_page(request):
-    return render(request, 'post-page.html', {})
+def post_page(request, pageNumber = 1, category = 'all', sortBy = 'newest'):
+    allPost = Post.objects.all()
+    paginator = Paginator(allPost, pageSize)
+    totalPage = paginator.num_pages
+    minPage = max(pageNumber - 2, 1)
+    maxPage = min(pageNumber + 2, totalPage)
+    rangePage = range(minPage, maxPage+1)
+    posts = paginator.get_page(pageNumber)
+    return render(request, 'post-page.html', {
+        "posts" : posts,
+        "rangePage": rangePage,
+        'currentPage': pageNumber
+    })
 
 @require_GET
-def post_view_page(request):
-    post = Post.objects.all()
-    data = {}
-    if len(post) > 0:
-        post = post[0]
-        data['post'] = post
-    print(post)
+def post_view_page(request, postId):
+    post = Post.objects.get(id = postId)
+    data = {
+        "post" : post
+    }
     return render(request, "post-view-page.html", data)
