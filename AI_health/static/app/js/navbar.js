af72eaf6b6aff0bee9081461e25f0f9aeece34f7
Mr kin <kiengithub@gmail.com>
2024-12-20 00:05:14 +0700

    thêm responsive cho navbar

--- AI_health/static/app/js/navbar.js
+++ AI_health/static/app/js/navbar.js
@@ -5,15 +5,4 @@ window.addEventListener("load", async () => {
     previousButton.addEventListener("click", () => {
         history.back()
     })
-
-    // const navbar = document.getElementById("main-navbar")
-    // const triggerScrollHeight = 100
-
-    // window.addEventListener("scroll", () => {
-    //     if (window.screenY > triggerScrollHeight) {
-    //         navbar.classList.add("fixed")
-    //     } else {
-    //         navbar.classList.remove("fixed")
-    //     }
-    // })
 })
